9f5d72c7699a21316bc117f6569b988bd0710b01
arpit-kalla <ak2242@cornell.edu>
2018-05-01 23:59:15 -0400

    Hotfix

--- AX5043/transmission.py
+++ AX5043/transmission.py
@@ -36,20 +36,20 @@ class AX5043():
 		print("Mode changed to Tranmitting")
 		# According to ERRATA for Silicon v51 
 		# turn off receiver
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_STANDBY)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_STANDBY.value)
 		usleep(100)
 		# release FIFO ports
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_FIFOON)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_FIFOON.value)
 		usleep(100)
 
 		# Set freqA and tune for TX
 		_ax5043.set_reg_tx()
 
 		# Clear FIFO data and flags
-		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x03)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT.value,0x03)
 
 		# FULL TX Mode
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_FULLTX)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_FULLTX.value)
 		usleep(100)
 
 		_ax5043.write_preamble();
@@ -58,51 +58,51 @@ class AX5043():
 		self.write_packets(data)
 
 		# Wait till Xtal is running
-		reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS)
+		reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS.value)
 		usleep(10)
-		while (reg != Register.AX_REG_XTALSTATUS_MASK):
-			reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS)
+		while (reg != Register.AX_REG_XTALSTATUS_MASK.value):
+			reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS.value)
 			usleep(10)
 		
 		# Commit FIFO
-		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x04)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT.value,0x04)
 
 		print("Transmiting...")
 		self.transmitted += 1
 		usleep(10)
 
 		# Wait till TX is done
-		reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
+		reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE.value)
 		usleep(10)
 		while (reg != 0x00):
-			reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
+			reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE.value)
 			usleep(10)
 		
 		print("TX done...Packet No : {}".format(self.transmitted))
 		
 		# Powerdown
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_POWERDOWN)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_POWERDOWN.value)
 		usleep(5000000)
 
 	def receive(self):
 		print("Mode changed to Receiving")
 		# Clear FIFO data and flags
-		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x03)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT.value,0x03)
 
 		# Set power mode to FULLRX
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_FULLRX)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_FULLRX.value)
 
 		print("Receiving...\n")
-		while (rstat != Register.AX_REG_RADIOSTATE_RX_MASK):
-			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
+		while (rstat != Register.AX_REG_RADIOSTATE_RX_MASK.value):
+			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE.value)
 		
-		while (rstat == Register.AX_REG_RADIOSTATE_RX_MASK):
-			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
+		while (rstat == Register.AX_REG_RADIOSTATE_RX_MASK.value):
+			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE.value)
 			usleep(100)
 		
 
-		fif0 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT0)
-		fif1 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT1) 
+		fif0 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT0.value)
+		fif1 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT1.value) 
 		FIFObytes = (fif1 << 8) | fif0
 
 		self.received += 1
@@ -112,7 +112,7 @@ class AX5043():
 		
 		print(" ... Packet No : {}".format(self.received))
 		# Set power mode to POWERDOWN
-		_ax5043.write_reg(Register.AX_REG_PWRMODE,Register.PWRMODE_POWERDOWN)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE.value,Register.PWRMODE_POWERDOWN.value)
 		return received_data 
 
 	def write_packets(self, data):
@@ -125,7 +125,7 @@ class AX5043():
 	def read_packets(self, FIFObytes):
 		data = ""
 		for i in range(FIFObytes):
-			reg = _ax5043.read_reg(Register.AX_REG_FIFODATA)	
+			reg = _ax5043.read_reg(Register.AX_REG_FIFODATA.value)	
 			# print(reg)
 			data += chr(reg)
 		return data
