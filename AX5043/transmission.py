2d621c1453f4801f0dbf394524da60056b22e046
arpit-kalla <ak2242@cornell.edu>
2018-05-01 23:53:11 -0400

    Hotfix

--- AX5043/transmission.py
+++ AX5043/transmission.py
@@ -12,21 +12,21 @@ class AX5043():
 
 		print("Initializing the Antenna")
 
-		status = _ax5043.read_reg(AX_REG_PWRMODE)
+		status = _ax5043.read_reg(Register.AX_REG_PWRMODE)
 		print("Power mode : {}".format(status))
 
-		version = _ax5043.read_reg(AX_REG_SILICONREVISION)
+		version = _ax5043.read_reg(Register.AX_REG_SILICONREVISION)
 		print("Silicon Revision : {}".format(version))
 
-		state = _ax5043.read_reg(AX_REG_RADIOSTATE)
+		state = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
 		print("Radio State : {}".format(state))
 
 		# turn off receiver
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_STANDBY)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_STANDBY)
 		usleep(100)
 
 		# release FIFO ports
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_FIFOON)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_FIFOON)
 		usleep(100)
 
 		# Init Transceiver
@@ -36,20 +36,20 @@ class AX5043():
 		print("Mode changed to Tranmitting")
 		# According to ERRATA for Silicon v51 
 		# turn off receiver
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_STANDBY)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_STANDBY)
 		usleep(100)
 		# release FIFO ports
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_FIFOON)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_FIFOON)
 		usleep(100)
 
 		# Set freqA and tune for TX
 		_ax5043.set_reg_tx()
 
 		# Clear FIFO data and flags
-		_ax5043.write_reg(AX_REG_FIFOSTAT,0x03)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x03)
 
 		# FULL TX Mode
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_FULLTX)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_FULLTX)
 		usleep(100)
 
 		_ax5043.write_preamble();
@@ -58,51 +58,51 @@ class AX5043():
 		self.write_packets(data)
 
 		# Wait till Xtal is running
-		reg = _ax5043.read_reg(AX_REG_XTALSTATUS)
+		reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS)
 		usleep(10)
-		while (reg != AX_REG_XTALSTATUS_MASK):
-			reg = _ax5043.read_reg(AX_REG_XTALSTATUS)
+		while (reg != Register.AX_REG_XTALSTATUS_MASK):
+			reg = _ax5043.read_reg(Register.AX_REG_XTALSTATUS)
 			usleep(10)
 		
 		# Commit FIFO
-		_ax5043.write_reg(AX_REG_FIFOSTAT,0x04)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x04)
 
 		print("Transmiting...")
 		self.transmitted += 1
 		usleep(10)
 
 		# Wait till TX is done
-		reg = _ax5043.read_reg(AX_REG_RADIOSTATE)
+		reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
 		usleep(10)
 		while (reg != 0x00):
-			reg = _ax5043.read_reg(AX_REG_RADIOSTATE)
+			reg = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
 			usleep(10)
 		
 		print("TX done...Packet No : {}".format(self.transmitted))
 		
 		# Powerdown
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_POWERDOWN)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_POWERDOWN)
 		usleep(5000000)
 
 	def receive(self):
 		print("Mode changed to Receiving")
 		# Clear FIFO data and flags
-		_ax5043.write_reg(AX_REG_FIFOSTAT,0x03)
+		_ax5043.write_reg(Register.AX_REG_FIFOSTAT,0x03)
 
 		# Set power mode to FULLRX
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_FULLRX)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_FULLRX)
 
 		print("Receiving...\n")
-		while (rstat != AX_REG_RADIOSTATE_RX_MASK):
-			rstat = _ax5043.read_reg(AX_REG_RADIOSTATE)
+		while (rstat != Register.AX_REG_RADIOSTATE_RX_MASK):
+			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
 		
-		while (rstat == AX_REG_RADIOSTATE_RX_MASK):
-			rstat = _ax5043.read_reg(AX_REG_RADIOSTATE)
+		while (rstat == Register.AX_REG_RADIOSTATE_RX_MASK):
+			rstat = _ax5043.read_reg(Register.AX_REG_RADIOSTATE)
 			usleep(100)
 		
 
-		fif0 = _ax5043.read_reg(AX_REG_FIFOCOUNT0)
-		fif1 = _ax5043.read_reg(AX_REG_FIFOCOUNT1) 
+		fif0 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT0)
+		fif1 = _ax5043.read_reg(Register.AX_REG_FIFOCOUNT1) 
 		FIFObytes = (fif1 << 8) | fif0
 
 		self.received += 1
@@ -112,7 +112,7 @@ class AX5043():
 		
 		print(" ... Packet No : {}".format(self.received))
 		# Set power mode to POWERDOWN
-		_ax5043.write_reg(AX_REG_PWRMODE,PWRMODE_POWERDOWN)
+		_ax5043.write_reg(Register.AX_REG_PWRMODE,PWRMODE_POWERDOWN)
 		return received_data 
 
 	def write_packets(self, data):
@@ -125,7 +125,7 @@ class AX5043():
 	def read_packets(self, FIFObytes):
 		data = ""
 		for i in range(FIFObytes):
-			reg = _ax5043.read_reg(AX_REG_FIFODATA)	
+			reg = _ax5043.read_reg(Register.AX_REG_FIFODATA)	
 			# print(reg)
 			data += chr(reg)
 		return data
